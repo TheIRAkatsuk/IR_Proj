5bb7c68d9ddc6abad0e432fcc9a52a0fe6b28b8a
Aftab <mohd.aftab571@gmail.com>
2019-11-28 00:45:41 +0100

    updated code

--- src/ovgu/ir/Main.java
+++ src/ovgu/ir/Main.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+		if (args!=null && (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0])))) {
 			System.out.println("Usage:\tjava -jar IR P.jar [path to document folder] [path to index folder] [VS/OK] [query]");
 			System.exit(0);
 		}
 
 		
-		String docsPath=Constants.FILE_PATH; //args[0];
-		String IndexPath = Constants.INDEX_PATH; //args[1];
+		String docsPath=Constants.FILE_PATH;//(args[0]!=null && !args[0].isEmpty())?args[0]:Constants.FILE_PATH;//Constants.FILE_PATH; //args[0];
+		String IndexPath =Constants.INDEX_PATH;//(args[0]!=null && !args[0].isEmpty())? args[1]:Constants.INDEX_PATH;//Constants.INDEX_PATH; //args[1];
 		boolean useBM25Okapi = true;//(args[2]!=null && !args[2].isEmpty() && args[2].equals("OK")) ? true : false;//true;//
 		String query ="";//(args[3]!=null && !args[3].isEmpty())?args[3]:""; //"";
 		List<Model> docList= new ArrayList<Model>();
